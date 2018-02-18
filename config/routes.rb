d44c58412bc210cc6d1ccf891e0d665da9abf320
EC2 Default User <ec2-user@ip-172-31-10-193.us-east-2.compute.internal>
2018-02-18 09:49:04 +0000

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DemoApp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
   resources :tasks
 
   # The priority is based upon order of creation: first created -> highest priority.
