12c32cb75515115042f5315b58ae3f425a6d1bcc
EC2 Default User <ec2-user@ip-172-31-10-193.us-east-2.compute.internal>
2018-02-19 06:05:55 +0000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+    
+    def full_title( page_title )
+        
+        # ページごとの完全なタイトルを返す
+        base_title = "Ruby on Rails Tutorial Sample App"
+        if page_title.empty?
+            base_title
+        else
+            "#{base_title} | #{page_title}"
+        end
+        
+        
+    end
+    
+    
 end
